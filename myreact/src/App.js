f26628ce289d4b5b23578817f8b7e9566f44b9a3
wagner.lopes <wagner.lopes@aluno.faculdadeimpacta.com.br>
2023-03-22 22:05:50 -0300

    react

---
+++ myreact/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TESTE</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum nisi ac lorem tincidunt, id faucibus nunc interdum. Curabitur ornare placerat eros, at consectetur felis. Donec eleifend at erat ut elementum. Nullam tortor nisi, lacinia vitae risus nec, tristique aliquam lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis rhoncus mattis purus id dignissim. Phasellus massa velit, lobortis in dapibus ut, volutpat vitae elit. Integer viverra vel massa sit amet vestibulum. Aenean consectetur egestas nunc, vel efficitur tellus porttitor id. Vivamus malesuada tincidunt nibh, id ultricies nunc pharetra blandit.</p>
+      <img src="https://coodesh.com/blog/wp-content/uploads/2021/03/papel-de-um-fullstack-scaled.jpg" width="500px" ></img>
+      <br></br><button> Click Here!</button>
+    </div>
+  );
+}
+
+export default App;
